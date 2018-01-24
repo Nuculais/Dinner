e867b98a4a20c6eb946c9228d4f5bbc38cc87446
Malin Augustsson <Malin Augustsson>
2018-01-24 15:29:17 +0100

    Some more from Malin

--- js/view/SelectDishView.js
+++ js/view/SelectDishView.js
@@ -1,1 +1,5 @@
 //Select Dish View
+
+
+var showdish1 = container.find("#dish1");
+//The dishes shown. However, these would probbly need to be procedurally generated somehow as the could be infinite dishes?
