4c4842ab42e806c4547025392a6e91c44a0f675f
Malin Augustsson <malinau@kth.se>
2018-02-21 14:10:45 +0100

    ...

---
+++ js/Controllers.js
@@ -0,0 +1,71 @@
+
+
+//According to the comments in app.js, we are not allowed to use jquery selectors
+//here, but according to the comments on lab 2 we have to have the controllers outside app.js yet not use container.find.
+//So what is left to use?
+
+//Controllers
+var SidebarDishController = function(view, model)
+{
+  $('plusGuest').click(function(){ //works
+  model.setNumberOfGuests(model.getNumberOfGuests() + 1);
+  });
+
+  $('minusGuest').click(function(){
+  model.setNumberOfGuests(model.getNumberOfGuests() - 1);
+});
+  //Display the dish name/s as well
+}
+
+var FindDishController = function(view, model)
+{
+  view.dinconfirm.click(function(){ //undefined
+    showFindDish();
+  });
+
+  view.searchdishes.click(function(){
+    var type= view.container.find('.dropdown').on('show.bs.dropdown', function(grej){
+      var thetype = $(grej.relatedTarget).text();
+      return thetype;
+    });
+
+    var filter = view.container.find('#searchForm').value.toLowerCase();
+
+    model.getAllDishes(type.toLowerCase(), filter);
+
+  });
+};
+
+var dinnerDetailsController = function(view, model)
+{
+  view.dinEdit.click(function(){
+  showFindDish();
+  });
+
+  view.addConfirm.click(function(){
+  var dishID = container.find('#dishname').value; //Assuming this is the dish's ID. Otherwise: (container.find('#dishname').value.toLowerCase()).id;
+  model.addDishToMenu(dishID);
+  showFindDish();
+  });
+};
+
+
+
+
+var dinnerOverviewController = function(view, model)
+{
+  view.goBackAndEditDinner.click(function(){
+    showFindDish();
+  });
+
+  view.dinprint.click(function(){
+    showDinnerPrintout();
+  });
+};
+
+var dinnerPrintoutController = function(view, model)
+{
+  view.dinedit.click(function(){
+    showFindDish();
+  });
+};
--- js/app.js
+++ js/app.js
@@ -15,9 +15,7 @@ $(function() {
 	var dinnerPrintoutView = new DinnerPrintoutView($("#dinnerprintoutView"), model);
 	var dinnerPrintoutController = new DinnerPrintoutController(dinnerPrintoutView, model);
 
-console.log(SidebarDishController);
-
-	//This works but not the controllers?
+	//This does not work
 	$('#newDinner').click(function(){
 		$('#indexScreen').toggleClass('hidden');
 		$('#selectdinnerView').toggleClass('hidden');
@@ -25,71 +23,6 @@ console.log(SidebarDishController);
 		//showSidebar();
 		//showFindDish();
 	});
-
-
-	//controllers
-	var SidebarDishController = function(view, model)
-	{
-	  view.plusGuest.click(function(){
-	  model.setNumberOfGuests(model.getNumberOfGuests() + 1);
-	  });
-
-	  view.minusGuest.click(function(){
-	  model.setNumberOfGuests(model.getNumberOfGuests() - 1);
-	});
-	  //Display the dish name/s as well
-	}
-
-	var FindDishController = function(view, model)
-	{
-	  view.dinconfirm.click(function(){
-	    showFindDish();
-	  });
-
-	  view.searchdishes.click(function(){
-	    var type= view.container.find('.dropdown').on('show.bs.dropdown', function(grej){
-	      var thetype = $(grej.relatedTarget).text();
-	      return thetype;
-	    });
-
-	    var filter = view.container.find('#searchForm').value.toLowerCase();
-
-	    model.getAllDishes(type.toLowerCase(), filter);
-
-	  });
-	};
-
-	var dinnerDetailsController = function(view, model)
-	{
-	  view.dinEdit.click(function(){
-	  showFindDish();
-	  });
-
-	  view.addConfirm.click(function(){
-	  var dishID = container.find('#dishname').value; //Assuming this is the dish's ID. Otherwise: (container.find('#dishname').value.toLowerCase()).id;
-	  model.addDishToMenu(dishID);
-	  showFindDish();
-	  });
-	};
-
-
-	var dinnerOverviewController = function(view, model)
-	{
-	  view.goBackAndEditDinner.click(function(){
-	    showFindDish();
-	  });
-
-	  view.dinprint.click(function(){
-	    showDinnerPrintout();
-	  });
-	};
-
-	var dinnerPrintoutController = function(view, model)
-	{
-	  view.dinedit.click(function(){
-	    showFindDish();
-	  });
-	};
 
 
 
